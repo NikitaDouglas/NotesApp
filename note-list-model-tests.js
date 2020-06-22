120c808fb42a704eefd052bdf00cc0ae0676e8b6
Nikita <latinisnotdead07@gmail.com>
2020-06-22 17:54:25 +0100

    NoteList can #createAndStore() Note instances

--- note-list-model-tests.js
+++ note-list-model-tests.js
@@ -4,4 +4,10 @@ function hasAnArrayOfNotes() {
     assert.isTrue(testNoteList.list.length === 0)
 }
 
+function canCreateAndStoreNote() {
+    testNoteList.createAndStore("Favourite drink: Diet Coke")
+    assert.isTrue(testNoteList.list.length === 1)
+}
+
 hasAnArrayOfNotes()
+canCreateAndStoreNote()
--- note-list-model.js
+++ note-list-model.js
@@ -1,3 +1,15 @@
 function NoteList() {
     this.list = []
 }
+
+NoteList.prototype = (function(){
+
+    function createAndStore(text) {
+        this.list.push(new Note(text))
+    }
+
+    return {
+        createAndStore: createAndStore
+    }
+
+})();
