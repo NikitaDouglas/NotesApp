ff9800c805964f630304d9704e41e80b56ba6fb3
Nikita <latinisnotdead07@gmail.com>
2020-06-22 16:55:53 +0100

    refactors test to use global variables

--- note-model-tests.js
+++ note-model-tests.js
@@ -1,6 +1,7 @@
+var testString = "My favourite language is JavaScript!"
+var testNote = new Note(testString);
+
 function testNoteInstantiatesWithText() {
-    var testString = "My favourite language is JavaScript!"
-    var testNote = new Note(testString);
     assert.isTrue(testNote.text === testString)
 }
 
