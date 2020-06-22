c23c59f81abc28bb3f31ab5f5bd2b742ec738d29
Nikita <latinisnotdead07@gmail.com>
2020-06-22 17:37:31 +0100

    refactors test to use a global variable

--- note-list-model-tests.js
+++ note-list-model-tests.js
@@ -1,6 +1,7 @@
+var testNoteList = new NoteList();
+
 function hasAnArrayOfNotes() {
-    var testNoteList = new NoteList();
-        assert.isTrue(testNoteList.list.length === 0)
+    assert.isTrue(testNoteList.list.length === 0)
 }
 
 hasAnArrayOfNotes()
