943ba339e10031fb8fd0e34dffd66fa09ee94865
Nikita <latinisnotdead07@gmail.com>
2020-06-23 11:07:55 +0100

    changes indentation

--- spec/note-model-tests.js
+++ spec/note-model-tests.js
@@ -2,11 +2,11 @@ var testString = "My favourite language is JavaScript!"
 var testNote = new Note(testString);
 
 function testNoteInstantiatesWithText() {
-    assert.isTrue(testNote.text === testString)
+  assert.isTrue(testNote.text === testString)
 }
 
 function testReturnText() {
-    assert.isTrue(testNote.returnText() === testString)
+  assert.isTrue(testNote.returnText() === testString)
 }
 
 testNoteInstantiatesWithText()
